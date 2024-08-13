a2dac9108824d08d7a3132c449c9b6c113bb33d7
Gopi2466 <gopi2466@gmail.com>
2024-08-13 16:41:04 -0400

    adding router

---
+++ Backend/routes/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const router = express.Router();
+
+module.exports = router;
+
+
+// In this file we are exporting a router and which ever
+// file needs it can use it.
