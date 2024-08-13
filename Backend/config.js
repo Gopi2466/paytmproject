af6061472afdcccc983218a92ea1552a96124fcc
Gopi2466 <gopi2466@gmail.com>
2024-08-13 16:53:37 -0400

    Final template setup done

---
+++ Backend/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    JWT_SECRET: "911GT3RS-porsche"
+}
--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
 const router = express.Router();
-
+const userRouter = require("./user")
 module.exports = router;
 
+
+router.use("/user", userRouter);
 
 // In this file we are exporting a router and which ever
 // file needs it can use it.
