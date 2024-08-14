22b891d3b400dbf7be215721e44574eeeef1e874
Gopi2466 <gopi2466@gmail.com>
2024-08-13 21:16:46 -0400

    react project setup

--- Frontend/paytm/src/App.jsx
+++ Frontend/paytm/src/App.jsx
@@ -1,13 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import {BrowserRouter, Routes, Route } from "react-router-dom"
+import { Signup } from './Components/Signup'
+import { Signin } from './Components/Signin'
+import { Dashboard } from './Components/Dashboard'
+import { SendMoney } from './Components/SendMoney'
 
 function App() {
   
 
   return (
     <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element ={<Signup />}></Route>
+        <Route path="/signin" element ={<Signin />}></Route>
+        <Route path="/dashboard" element ={<Dashboard />}></Route>
+        <Route path="/send" element ={<SendMoney />}></Route>
+      </Routes>
+    
+    </BrowserRouter>
       
     </>
   )
