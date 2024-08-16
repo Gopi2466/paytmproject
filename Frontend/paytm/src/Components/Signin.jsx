40e5f08f6cf0f47c027b6bcc69030c763f3938dc
Gopi2466 <gopi2466@gmail.com>
2024-08-16 02:15:58 -0400

    signup and signin connection with backend

--- Frontend/paytm/src/Components/Signin.jsx
+++ Frontend/paytm/src/Components/Signin.jsx
@@ -1,19 +1,32 @@
+import axios from "axios";
 import { BottomWarning } from "./BottomWarning";
 import { Button } from "./Button";
 import { Heading } from "./Heading";
 import { InputBox } from "./InputBox";
 import { InputLabel } from "./inputLabel";
+import { useState } from "react";
 
 export function Signin() {
+    const [userSignin, setUserSignin] = useState("")
+    const [userPassword, setUserPassword] = useState("")
+    async function fetchSignin() {
+
+        const response = await axios.post("http://localhost:3000/api/v1/user/signin",{
+            username: userSignin,
+            password: userPassword
+        })
+        console.log(response.data)
+    }
+
     return<div className="bg-slate-300 h-screen flex justify-center ">
         <div className="flex flex-col justify-center">
             <div className="rounded-lg bg-white w-80 p-2 h-max px-4">
         <Heading label={"Signin"} subHeading={"Enter your credentials to access your account"}></Heading>
         <InputLabel label={"Email"}></InputLabel>
-        <InputBox label={"email@eamil.com"}></InputBox>
+        <InputBox label={"email@eamil.com"} inputValue={setUserSignin}></InputBox>
         <InputLabel label={"password"}></InputLabel>
-        <InputBox label={"password"}></InputBox>
-        <Button label="Sign In"/>
+        <InputBox label={"password"} inputValue={setUserPassword}></InputBox>
+        <Button label="Sign In" onClick={fetchSignin}/>
         <BottomWarning label={"Don't have an account?"} buttonText={"Sign Up"} to={"/signup"}/>
         </div>
         </div>
--- Frontend/paytm/src/Components/Signup.jsx
+++ Frontend/paytm/src/Components/Signup.jsx
@@ -5,28 +5,23 @@ import { InputBox } from "./InputBox"
 import { Signin } from "./Signin"
 import { InputLabel } from "./inputLabel"
 import axios from "axios"
+import { useState } from "react"
 
 export function Signup() {
-    const [signupObject, setSignupObject] = useState({
-        username: "",
-        password:"",
-        firstName: "",
-        lastName:""
-    })
+    const [signupUsername, setSignupUsername] = useState("")
+    const [signupFirstName, setSignupFirstName] = useState("")
+    const [signupLastName, setSignupLastName] = useState("")
+    const [signupPassword, setSignupPassword] = useState("")
+       // how can I create a sigle state variable to update all teh values
 
     async function fetchSignup() {
-       const response =  await axios.post("http://localhost:3000/app/v1/user/signup", {
-                username: signupObject.username,
-                password: signupObject.password,
-                firstName: signupObject.firstName,
-                lastName: signupObject.lastName
-    },
-            { headers: {
-                'Content-Type': "application/json",
-                "Authorization": "Token value here"
-            },
-            
-        });
+        console.log(signupFirstName, signupLastName, signupUsername, signupPassword)
+       const response =  await axios.post("http://localhost:3000/api/v1/user/signup", {
+                username: signupUsername,
+                password: signupPassword,
+                firstName: signupFirstName,
+                lastName: signupLastName
+    });
 
         console.log(response.data);
     }
@@ -38,12 +33,14 @@ export function Signup() {
         <div className="shadow-md shadow-black bg-white rounded-lg h-max w-80 px-4" >
             <Heading label="Sign Up" subHeading={"Enter your information to create an account"}/>
             <InputLabel label="First Name" />
-            <InputBox label= "Gopi" inputValue={setSignupObject}/>
+            <InputBox label= "Gopi" inputValue={setSignupFirstName}/>
             <InputLabel label="Last Name" />
-            <InputBox label="Singh" inputValue={setSignupObject}/>
+            <InputBox label="Singh" inputValue={setSignupLastName}/>
+            <InputLabel label="Email" />
+            <InputBox label="Singh" inputValue={setSignupUsername}/>
             <InputLabel label="Password" />
-            <InputBox label="password" inputValue={setSignupObject}/>
-    <div ><Button onClick={fetchData} label= "Sign Up"/></div>
+            <InputBox label="password" inputValue={setSignupPassword}/>
+    <div ><Button onClick={fetchSignup} label= "Sign Up"/></div>
     <BottomWarning label={"Already have an account?"} buttonText={"Login"} to={"/signin"}/>
     </div>
     </div>
