08a3779ff16d3d7cf0708b3a3485ddda913d89fa
Gopi2466 <Gopi2466@gmail.com>
2024-08-16 00:42:21 -0400

    check useState functionality with signup page

--- Frontend/paytm/src/Components/InputBox.jsx
+++ Frontend/paytm/src/Components/InputBox.jsx
@@ -1,4 +1,8 @@
-export function InputBox({label}) {
-    return <input className="border p-2 rounded-md w-full" type="text" placeholder={label}></input>
+export function InputBox({label, inputValue}) {
+    return <input className="border p-2 rounded-md w-full" type="text" placeholder={label} onChange={(e) => {
+        inputValue(e.target.value)
+    }}></input>
     
 }
+
+// How can a child component send something back to the main component.
--- Frontend/paytm/src/Components/Signup.jsx
+++ Frontend/paytm/src/Components/Signup.jsx
@@ -4,19 +4,46 @@ import { Heading } from "./Heading"
 import { InputBox } from "./InputBox"
 import { Signin } from "./Signin"
 import { InputLabel } from "./inputLabel"
+import axios from "axios"
 
 export function Signup() {
+    const [signupObject, setSignupObject] = useState({
+        username: "",
+        password:"",
+        firstName: "",
+        lastName:""
+    })
+
+    async function fetchSignup() {
+       const response =  await axios.post("http://localhost:3000/app/v1/user/signup", {
+                username: signupObject.username,
+                password: signupObject.password,
+                firstName: signupObject.firstName,
+                lastName: signupObject.lastName
+    },
+            { headers: {
+                'Content-Type': "application/json",
+                "Authorization": "Token value here"
+            },
+            
+        });
+
+        console.log(response.data);
+    }
+
+
+
     return <div className="flex justify-center h-screen bg-slate-300">
         <div className="flex justify-center flex-col">
         <div className="shadow-md shadow-black bg-white rounded-lg h-max w-80 px-4" >
             <Heading label="Sign Up" subHeading={"Enter your information to create an account"}/>
             <InputLabel label="First Name" />
-            <InputBox label="Gopi"/>
+            <InputBox label= "Gopi" inputValue={setSignupObject}/>
             <InputLabel label="Last Name" />
-            <InputBox label="Singh"/>
+            <InputBox label="Singh" inputValue={setSignupObject}/>
             <InputLabel label="Password" />
-            <InputBox label="password"/>
-    <div ><Button label= "Sign Up"/></div>
+            <InputBox label="password" inputValue={setSignupObject}/>
+    <div ><Button onClick={fetchData} label= "Sign Up"/></div>
     <BottomWarning label={"Already have an account?"} buttonText={"Login"} to={"/signin"}/>
     </div>
     </div>
